d9bcf7cee5bd20b241144b3b6eaa56de54e681f7
Tedpotenzo93 <dennis-wahl@outlook.de>
2018-04-25 15:11:05 +0200

    Test Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { NewPage } from '../new/new'
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  
+  next() {
+	  this.navCtrl.push(NewPage);
+  }
+  
+  
+
+}
