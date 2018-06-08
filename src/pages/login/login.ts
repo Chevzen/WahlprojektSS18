e0ce61131b0a797c3c25c3ff1922a3d130741452
PatrickGammersbach <gammersbach-patrick@t-online.de>
2018-06-08 09:15:38 +0200

    Model-Versuch

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -418,8 +418,9 @@ export class LoginPage {
 			header.style.display = "none";
 			var login: HTMLElement = document.getElementById('login');
 			login.style.display = "block";
-			//loginFunction(this);
-			this.navCtrl.setRoot(HomePage);
+			loginFunction(this);
+			//Falls man beim Starten der App nicht den Login machen möchte einfach die loginFunction auskommentieren und diese Zeile einkommentieren:
+			//this.navCtrl.setRoot(HomePage);
 		}
   }
 
@@ -439,7 +440,7 @@ export class LoginPage {
       }
 
 
-			//loginFunction(this);
+			loginFunction(this);
 			var ladeicon: HTMLElement = document.getElementById('laden');
 			ladeicon.style.display ="block";
 
