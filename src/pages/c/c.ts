d2a0d689b4a06f6793226ff9fe809b8c2cdf036d
Felix Holtkoetter <f.holtkoetter@gmx.de>
2018-06-15 14:01:47 +0200

    Bissle Aufgeräumt

--- src/pages/c/c.ts
+++ src/pages/c/c.ts
@@ -115,34 +115,6 @@ function parseToCampus(){
   return campus;
 }
   
-function giveWochentag(){
-  var jetzt = new Date();
-  switch(jetzt.getDay()){
-    case 1: return "Montag";
-    case 2: return "Dienstag";
-    case 3: return "Mittwoch";
-    case 4: return "Donnerstag";
-    case 5: return "Freitag";
-    case 6: return "Samstag";
-    case 0: return "Sonntag";
-    default: break;
-    }
-  }
-
-function giveUhrzeit(){
-    var jetzt = new Date();
-    var stunden = jetzt.getHours();
-    var minuten = jetzt.getMinutes();
-    switch(true){
-      case (stunden == 8 && minuten >= 5 || stunden == 9 && minuten <= 45): return "8:15:00";  
-      case (stunden == 9 && minuten >= 45 || stunden == 10 || stunden == 11  && minuten <= 30): return "10:00:00";
-      case (stunden == 11 && minuten >= 30 || stunden == 12 || stunden == 13 && minuten <= 15): return "11:45:00";
-      case (stunden == 14 && minuten >= 15 || stunden == 15 && minuten <= 45): return "14:15:00";
-      case (stunden == 16 || stunden == 17 && minuten <= 30): return "16:00:00";
-      case (stunden == 17 && minuten >= 45 || stunden == 18 || stunden == 19 && minuten <= 15): return "17:45:00";
-      default: break;
-    }
-}
 
 @IonicPage()
 @Component({
